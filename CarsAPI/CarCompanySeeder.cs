b6250e4547b8ccdcc635d021ec5f07d08c1eff1d
Bartosz Michalski <bartek10114@wp.pl>
2021-08-23 20:16:00 +0200

    dodanie tabeli z uzytkownikami i rolami

---
+++ CarsAPI/CarCompanySeeder.cs
@@ -0,0 +1,51 @@
+﻿using CarsAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarsAPI
+{
+    public class CarCompanySeeder
+    {
+        private readonly CarDbContext context;
+
+        public CarCompanySeeder(CarDbContext context)
+        {
+            this.context = context;
+        }
+        public void Seed()
+        {
+            if (context.Database.CanConnect())
+            {
+                if (!context.Roles.Any())
+                {
+                    var roles = GetRoles();
+                    context.Roles.AddRange(roles);
+                    context.SaveChanges();
+                }
+            }
+
+        }
+
+        private IEnumerable<Role> GetRoles()
+        {
+            var roles = new List<Role>()
+            {
+                new Role()
+                {
+                    Name = "User"
+                },
+                new Role()
+                {
+                    Name = "Admin"
+                },
+                new Role()
+                {
+                    Name = "Mod"
+                }
+            };
+            return roles;
+        }
+    }
+}
