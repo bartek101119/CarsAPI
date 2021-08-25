66a23c6a85145221d83fba3fd05465cece3fb835
Bartosz Michalski <bartek10114@wp.pl>
2021-08-25 17:38:54 +0200

    dodanie możliwości logowania się przy pomocy tokenów JWT

---
+++ CarsAPI/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using CarsAPI.Exceptions;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarsAPI.Middleware
+{
+    public class ErrorHandlingMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next.Invoke(context);
+            }
+            catch(BadRequestException badRequestException)
+            {
+                context.Response.StatusCode = 400;
+                await context.Response.WriteAsync(badRequestException.Message);
+            }
+            catch(Exception)
+            {
+                context.Response.StatusCode = 500;
+                await context.Response.WriteAsync("Something went wrong");
+            }
+        }
+    }
+}
